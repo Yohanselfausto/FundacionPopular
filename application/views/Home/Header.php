a75bd6bbde9e51d10fb3f1d9d43b5f0d74eceab9
YohanselFausto <YohanselFausto>
2016-08-13 00:07:27 -0400

    new changes

---
+++ application/views/Home/Header.php
@@ -0,0 +1,67 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Fundación Universidad Popular Inc.</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="author" content="Fausto Jesús De La Cruz Caminero" />
+		<meta name="description" content="La Fundación Universidad Popular Inc. se crea y organiza exclusivamente con fines 
+		de promover el desarrollo social de las comunidades más vulnerables." /> 
+		<meta name="keywords" content="República Dominicana, Fundación, Educación, Social,
+		economía, Derecho, Leyes, Abogado, Salud, justicia, humano, bienestar, valores, estudiantes
+		cursos, GRATIS, FREE," />
+
+		<link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:700italic,400,300,700' rel='stylesheet' type='text/css'>	
+        <link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet">
+			<link rel="stylesheet" href="<?php echo base_url();?>/assets/css/skel-noscript.css" >
+			<link rel="stylesheet" href="<?php echo base_url();?>/assets/css/style.css" >
+			<link rel="stylesheet" href="<?php echo base_url();?>/assets/css/style-desktop.css" >
+		
+		<!--[if lte IE 8]><link rel="stylesheet" href="<?php echo base_url();?>/assets/css/ie/v8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="<?php echo base_url();?>/assets/css/ie/v9.css" /><![endif]-->
+		<!--[if lte IE 8]><script src="<?php echo base_url();?>/assets/js/html5shiv.js"></script><![endif]-->
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+		
+	</head>
+	<body class="homepage">
+
+	<!-- Header -->
+		<div id="header">
+			<div class="container">
+					
+				<!-- Logo -->
+					<div id="logo">
+						<h1><a href="#">Ex Machina</a></h1>
+					</div>
+				
+				<!-- Nav -->
+					<nav id="nav">
+						<ul>
+							<li class="active"><a href=" ">Homepage</a></li>
+							<li><a href="#">Left Sidebar</a></li>
+							<li><a href="#">Right Sidebar</a></li>
+							<li><a href="#">No Sidebar</a></li>
+						</ul>
+					</nav>
+
+			</div>
+		</div>
+	<!-- Header -->
+
+	<!-- Banner -->
+		<div id="banner">
+			<div class="container">
+			</div>
+		</div>
+	<!-- /Banner -->
+
+
+	
+
+
+
+
+
+	
